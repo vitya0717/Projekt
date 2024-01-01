06a25cfa13400ba6a28ee8440bcb4a0a37276027
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2024-01-01 14:19:20 +0100

    final update

--- Projekt/projektfrontend/src/page-components/UserOrders.js
+++ Projekt/projektfrontend/src/page-components/UserOrders.js
@@ -36,11 +36,12 @@ const UserOrders = ({ setSeverityMessage, openAlert, setSeverity, userData, curr
   }, [counter])
   return (
     userData !== null ?
-      <div className="container d-flex justify-content-center">
-        {
-          orders.map((order) => (
-            console.log(order.orderId),
-              <div idvalue={order.orderId} id={"card"+order.orderId} key={"card"+order.orderId} className="card m-3" style={{ width: "18rem" }}>
+      (orders.length > 0 ?
+        <div className="container d-flex justify-content-center">
+          {
+            orders.map((order) => (
+              console.log(order.orderId),
+              <div idvalue={order.orderId} id={"card" + order.orderId} key={"card" + order.orderId} className="card m-3" style={{ width: "18rem" }}>
                 <div className="card-body">
                   <h5 className="card-title">Rendelés azonosító #{order.orderId}</h5>
                   <p className="card-text text-muted">Rendelés dátuma: {new Date(order.orderDate).getFullYear() + "-" + (new Date(order.orderDate).getMonth() + 1) + "-" + new Date(order.orderDate).getDate() + " " + new Date(order.orderDate).getHours() + ":" + new Date(order.orderDate).getMinutes() + ":" + new Date(order.orderDate).getSeconds()} {""}</p>
@@ -49,21 +50,20 @@ const UserOrders = ({ setSeverityMessage, openAlert, setSeverity, userData, curr
                 <ul className="list-group list-group-flush">
                   {order.orderedItems.map((orderedItem) => (
                     vegosszeg += orderedItem.item.productPrice * orderedItem.quantity,
-                    <li key={"list"+orderedItem.orderDeatilId} className="list-group-item">{orderedItem.item.productName}  <span className='fw-bold text-primary'>x{orderedItem.quantity}</span></li>
+                    <li key={"list" + orderedItem.orderDeatilId} className="list-group-item">{orderedItem.item.productName}  <span className='fw-bold text-primary'>x{orderedItem.quantity}</span></li>
                   )
                   )}
                 </ul>
                 <div className="card-body">
                   <p className="card-text">Összesen fizetendő: <span className='text-success'>{vegosszeg}Ft</span></p>
-                  <button id='orderDeleteButton' orderid={order.orderId} data-bs-toggle="modal" data-bs-target={"#orderDeleteModal"+order.orderId} href="#" className="btn btn-danger text-warning text-decoration-none">Rendelés visszavonás</button>
+                  <button id='orderDeleteButton' orderid={order.orderId} data-bs-toggle="modal" data-bs-target={"#orderDeleteModal" + order.orderId} href="#" className="btn btn-danger text-warning text-decoration-none">Rendelés visszavonás</button>
                   <DeleteOrderConfirmModal handleCounter={handleCounter} orderId={order.orderId} userData={userData} setSeverityMessage={setSeverityMessage} openAlert={openAlert} setSeverity={setSeverity} />
                 </div>
               </div>
             ))
-            
-        }
-      </div>
-      
+
+          }
+        </div> : <h2 className='text-warning text-center mt-5'>Még nincs egy rendelésed sem! :c</h2>)
       :
       (<div>
         <div style={{ position: "absolute", right: "50%", zIndex: "1", top: "50%", transform: "translateY(-50%, -50%)" }}>
